4e8fce276493b4e114377d498b735ec78534a663
Octavian <odavid@castlefineart.com>
2022-12-17 11:36:00 +0000

    [16] - Profile details

--- components/profile-page/CurrentProfile.tsx
+++ components/profile-page/CurrentProfile.tsx
@@ -22,12 +22,18 @@ export default function CurrentProfile() {
   }, [session])
 
   return (
-   <div className='flex items-center flex-col'>
+   <div className='flex items-center flex-col w-full'>
     <img src={user?.images[0]?.url} width="200" height="200" className='rounded-full w-[200px] h-[200px] object-cover object-center' />
-    {user && <div className='flex flex-col'>
-      <span>{user.display_name}</span>
-      <span>{user.email}</span>
-      <span>{user.followers.total} </span>
+      {user && <div className='flex flex-col w-full mt-5 gap-y-3'>
+        <div className='border rounded-md px-2 py-1 flex items-center justify-between w-full'>
+          Username: <span>{user.display_name}</span>
+        </div>
+        <div className='border rounded-md px-2 py-1 flex items-center justify-between w-full'>
+          Email: <span>{user.email}</span>
+        </div>
+        <div className='border rounded-md px-2 py-1 flex items-center justify-between w-full'>
+          Followers: <span>{user.followers.total} </span>
+        </div>
     </div>}
   </div>
   )
