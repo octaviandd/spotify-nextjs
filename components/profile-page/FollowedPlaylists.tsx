cc0973f301cea3e83286adce6a3d62aabf0f150a
Octavian <odavid@castlefineart.com>
2022-12-17 17:43:26 +0000

    [16] - Swiper buttons

--- components/profile-page/FollowedPlaylists.tsx
+++ components/profile-page/FollowedPlaylists.tsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Mousewheel } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -8,10 +8,15 @@ import 'swiper/css/scrollbar';
 import { useSession } from 'next-auth/react';
 import { getSpotifyData } from '../utils';
 import { Playlist } from '../../types/components';
+import { Swiper as SwiperCore } from 'swiper/types';
+
 
 export default function FollowedArtists() {
   const [currentPlaylists, setCurrentPlaylists] = useState();
   const { data: session } = useSession();
+    const swiperRef = useRef<SwiperCore>();
+    const prevButtonRef = useRef<HTMLButtonElement>(null);
+    const nextButtonRef = useRef<HTMLButtonElement>(null)
 
    useEffect(() => {
     session?.accessToken && getCurrentlyFollowed();
@@ -28,20 +33,33 @@ export default function FollowedArtists() {
   }
 
   return (
-    <div className='flex flex-col w-full mx-auto px-20 mt-20'>
-      <p className='text-xl mb-6'>Followed playlists</p>
-      <div className='w-[80vw] flex'>
+    <div className='flex flex-col w-full mx-auto mt-20'>
+      <div className='flex items-center justify-between mb-5 px-20'>
+        <p className='text-xl mb-6'>Followed playlists</p>
+        <div className=''>
+          <button onClick={() => swiperRef.current?.slidePrev()} ref={prevButtonRef} className="text-lg px-3 py-3 bg-[#F6F4F4] text-white w-[60px] h-[60px]">
+            <svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 32 32"><path d="M32 15H3.41l8.29-8.29-1.41-1.42-10 10a1 1 0 0 0 0 1.41l10 10 1.41-1.41L3.41 17H32z" data-name="4-Arrow Left"/></svg>
+          </button>
+          <button onClick={() => swiperRef.current?.slideNext()} ref={nextButtonRef} className="text-lg px-3 py-3 bg-[#F6F4F4] text-white w-[60px] h-[60px]">
+            <svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 32 32"><path d="m31.71 15.29-10-10-1.42 1.42 8.3 8.29H0v2h28.59l-8.29 8.29 1.41 1.41 10-10a1 1 0 0 0 0-1.41z" data-name="3-Arrow Right"/></svg>
+          </button>
+        </div>
+      </div>
+      <div className='w-[100vw] flex px-20'>
         <Swiper
           spaceBetween={25}
           slidesPerView={4}
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          navigation
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
+          slidesPerGroup={3}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
         >
           {currentPlaylists && currentPlaylists.map((playlist: Playlist, index: number) => (
             <SwiperSlide key={index}>
-              <img src={playlist.images[0].url} className="h-[250px] object-cover object-center cursor-grab rounded-lg"/>
+              <div className='flex flex-col'>
+                <img src={playlist.images[0].url} className="h-[250px] object-cover object-center cursor-grab rounded-lg" />
+                <span className='font-artists text-xl mt-4 leading-5 text-[#010101] font-medium tracking-[-0.2px]'>{playlist.name}</span>
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
