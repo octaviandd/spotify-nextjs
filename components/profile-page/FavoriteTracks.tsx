859ce7e2f14ae4f3dc173c43c3c47134841ee029
Octavian <odavid@castlefineart.com>
2022-12-19 13:38:48 +0000

    [41] - Add timers'

--- components/profile-page/FavoriteTracks.tsx
+++ components/profile-page/FavoriteTracks.tsx
@@ -47,16 +47,16 @@ export default function FavoriteTracks() {
       <div className='flex text-xl'>
         <p className='mb-6 text-xl'>Favorite Songs</p>
         <div className='ml-3'>
-          <span onClick={() => setCurrentLimit(10)}  className='border px-3 py-2 rounded-md cursor-pointer'>10</span>
+          <span onClick={() => setCurrentLimit(10)}  className={currentLimit == 10 ? 'bg-black text-white border px-3 py-2 rounded-md cursor-pointer ease-in-out' : 'border px-3 py-2 rounded-md cursor-pointer ease-in-out'}>10</span>
         </div>
         <div className='ml-3'>
-          <span onClick={() => setCurrentLimit(25)} className='border px-3 py-2 rounded-md cursor-pointer'>25</span>
+          <span onClick={() => setCurrentLimit(25)} className={currentLimit == 25 ? 'bg-black text-white border px-3 py-2 rounded-md cursor-pointer ease-in-out' : 'border px-3 py-2 rounded-md cursor-pointer ease-in-out'}>25</span>
         </div>
         <div className='ml-3'>
-          <span onClick={() => setCurrentLimit(50)} className='border px-3 py-2 rounded-md cursor-pointer'>50</span>
+          <span onClick={() => setCurrentLimit(50)} className={currentLimit == 50 ? 'bg-black text-white border px-3 py-2 rounded-md cursor-pointer ease-in-out' : 'border px-3 py-2 rounded-md cursor-pointer ease-in-out'}>50</span>
         </div>
          <div className='ml-3'>
-          <span onClick={() => getTopOneHundred()} className='border px-3 py-2 rounded-md cursor-pointer'>100</span>
+          <span onClick={() => getTopOneHundred()} className={currentLimit == 100 ? 'bg-black text-white border px-3 py-2 rounded-md cursor-pointer ease-in-out' : 'border px-3 py-2 rounded-md cursor-pointer ease-in-out'}>100</span>
         </div>
       </div>
       <div className='grid-cols-2 grid grid-rows-auto gap-y-1 gap-x-3'>
---
+++ components/profile-page/SwiperButtons.tsx
@@ -0,0 +1,19 @@
+import React, { forwardRef } from 'react';
+
+type Props = {
+  swiperRef: any
+}
+type Ref = HTMLDivElement;
+
+export const SwiperButtons = forwardRef<Ref, Props>((props, ref) => {
+  return (
+    <div>
+      <button onClick={() => props.swiperRef.current?.slidePrev()} ref={prevButtonRef} className="text-lg px-3 py-3 bg-[#F6F4F4] text-white w-[60px] h-[60px]">
+        <svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 32 32"><path d="M32 15H3.41l8.29-8.29-1.41-1.42-10 10a1 1 0 0 0 0 1.41l10 10 1.41-1.41L3.41 17H32z" data-name="4-Arrow Left"/></svg>
+      </button>
+      <button onClick={() => props.swiperRef.current?.slideNext()} ref={nextButtonRef} className="text-lg px-3 py-3 bg-[#F6F4F4] text-white w-[60px] h-[60px]">
+        <svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 32 32"><path d="m31.71 15.29-10-10-1.42 1.42 8.3 8.29H0v2h28.59l-8.29 8.29 1.41 1.41 10-10a1 1 0 0 0 0-1.41z" data-name="3-Arrow Right"/></svg>
+      </button>
+    </div>
+  )
+})
