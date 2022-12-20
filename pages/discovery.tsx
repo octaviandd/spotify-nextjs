1299a11f22371d69ad7f4024d4cae25662e76ae6
Octavian <odavid@castlefineart.com>
2022-12-20 13:48:31 +0000

    [17] - Featured categories

--- pages/discovery.tsx
+++ pages/discovery.tsx
@@ -5,6 +5,7 @@ import { getSpotifyData } from '../components/utils';
 import { useSession } from 'next-auth/react';
 import FeaturedPlaylists from '../components/discovery-page/FeaturedPlaylists';
 import FeaturedAlbums from '../components/discovery-page/FeaturedAlbums';
+import FeaturedCategories from '../components/discovery-page/FeaturedCategories';
 
 export default function Discovery() {
   const { data: session } = useSession();
@@ -13,6 +14,7 @@ export default function Discovery() {
     <Layout>
       <FeaturedPlaylists></FeaturedPlaylists>
       <FeaturedAlbums></FeaturedAlbums>
+      <FeaturedCategories></FeaturedCategories>
       {/* <FadeInOut>
       </FadeInOut> */}
     </Layout>
