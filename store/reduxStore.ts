bb1bbb5f09b5d3b7bfa62e8f67698700be9fe846
Octavian <odavid@castlefineart.com>
2022-12-31 13:23:44 +0000

    [main] - Missed changes

--- store/reduxStore.ts
+++ store/reduxStore.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit"
-import rootReducer from "./store/rootReducers"
+import rootReducer from "./rootReducers"
 
 const store = configureStore({
   reducer: rootReducer,
